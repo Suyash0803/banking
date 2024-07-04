d8a3a792498b23182d65d3e0caf17cb04ae52f92
suyash <suyash08cr7@gmail.com>
2024-07-04 12:29:41 +0530

    Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,29 @@
-import HeaderBox from '@/components/HeaderBox/page'
-import React from 'react'
-import BalanceBox from '@/components/BalanceBox';
-const Home= () => {
-  const loggedIn = {firstName: 'John'};
+import HeaderBox from "@/components/HeaderBox/page";
+import React from "react";
+import BalanceBox from "@/components/BalanceBox";
+import RightSideBar from "@/components/RightSideBar";
+const Home = () => {
+  const loggedIn = { firstName: "John" ,lastName:"Doe",email:"suyash@gmail.com"};
   return (
-    <section className='home'>
+    <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName} subtext="Access and manage ur account"/>
-          <BalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}/>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName}
+            subtext="Access and manage ur account"
+          />
+          <BalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
         </header>
       </div>
+        <RightSideBar user={loggedIn} transactions={[]} banks={[{currentBalance:1234},{currentBalance:1234}]}/>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
